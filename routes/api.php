89c7d4b5dfd5d93ebbca57d5541054610e7458de
Flaviano Honorato <flaviano.honorato@gmail.com>
2022-07-06 10:34:04 -0300

    force json return

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\CsvController;
+use App\Http\Middleware\ForceJsonMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::post('send-data-to-job', [CsvController::class, 'sendDataToJob']);
+Route::post('send-data-to-job', [CsvController::class, 'sendDataToJob'])
+    ->middleware(ForceJsonMiddleware::class);
